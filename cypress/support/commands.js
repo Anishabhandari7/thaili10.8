6f64201b56a1cf6fc813da2a5655e46df417daa2
AnishaBhandari <97871488+Anishabhandari7@users.noreply.github.com>
2023-01-23 15:21:11 +0545

    Delete cypress directory

--- cypress/support/commands.js
+++
@@ -1,7 +0,0 @@
-Cypress.Commands.add('login', (text, password) => {
-cy.visit("")
-
-cy.get("#user_name").type(text)
-cy.get("#user_password").type(password)
-cy.get('#loginform > .btn').click()
-})
